b717deaf28c058beaa51f687419536026d905fc1
matthiasertl <matthias.ertl@gmail.com>
2019-04-29 10:01:02 +0200

    Update Ping.groovy

--- devicetypes/mightysmartthings/Ping.groovy
+++ devicetypes/mightysmartthings/Ping.groovy
@@ -1,7 +1,7 @@
 /**
  *  HTTP Ping
  *
- *  Copyright 2014 Kristopher Kubicki
+ *  2018 Matthias Ertl
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
