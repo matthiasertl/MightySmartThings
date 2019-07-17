98d5a3a326e805455d9ef50889540156ce02955d
matthiasertl <matthias.ertl@gmail.com>
2019-07-17 14:01:39 +0200

    Update ping-monitor-nowol.groovy

--- devicetypes/matthias-ertl/ping-monitor-nowol.src/ping-monitor-nowol.groovy
+++ devicetypes/matthias-ertl/ping-monitor-nowol.src/ping-monitor-nowol.groovy
@@ -1,7 +1,6 @@
 /**
  *  HTTP Ping
  *
- *  Copyright 2014 Kristopher Kubicki
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
